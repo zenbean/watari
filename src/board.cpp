22424fb6ef1e667d96c6f4a96f633b7e7c9848f6
carys <caryshcleung@gmail.com>
2026-04-19 23:13:35 +0100

    copying board states in move logic shenaniganmight have to separate ValidMove logic into different functions

--- src/board.cpp
+++ src/board.cpp
@@ -27,67 +27,67 @@ int Board::CoordinateToIndex(const int& x, const int& y){
     return y * Board::boardSize + x;
 }
 
-void Board::FindLiberties(const int& n, const std::optional<Stone>& colour, std::unordered_set<int>& liberties, std::unordered_set<int>& visitedPositions){
+void Board::FindLiberties(const int& n, const std::optional<Stone>& colour, std::unordered_set<int>& liberties, std::unordered_set<int>& visitedPositions, std::vector<std::optional<Stone>>& local_board){
     if(colour==std::nullopt) return;
     if(!visitedPositions.insert(n).second) return; // check if node is visited
 
     for (int neighbourPos:neighbours[n]){ // check neighbouring stones
-        if (board[neighbourPos]==std::nullopt){ // empty spot?
+        if (local_board[neighbourPos]==std::nullopt){ // empty spot?
             liberties.insert(neighbourPos);
         }
-        else if (board[neighbourPos]==colour){
-            FindLiberties(neighbourPos, colour, liberties, visitedPositions);
+        else if (local_board[neighbourPos]==colour){
+            FindLiberties(neighbourPos, colour, liberties, visitedPositions, local_board);
         }
     }
 }
 
-void Board::FindGroup(const int&n, const std::optional<Stone>& colour, std::unordered_set<int>& groupPositions, std::unordered_set<int>& visitedPositions){
+void Board::FindGroup(const int&n, const std::optional<Stone>& colour, std::unordered_set<int>& groupPositions, std::unordered_set<int>& visitedPositions, std::vector<std::optional<Stone>>& local_board){
     if(colour==std::nullopt) return;
     if(!visitedPositions.insert(n).second) return; // check if node is visited
     groupPositions.insert(n);
     for (int neighbourPos:neighbours[n]){ // check neighbouring stones
-        if (board[neighbourPos]==colour){
+        if (local_board[neighbourPos]==colour){
             groupPositions.insert(neighbourPos);
-            FindGroup(neighbourPos, colour, groupPositions, visitedPositions);
+            FindGroup(neighbourPos, colour, groupPositions, visitedPositions, local_board);
         }
     }
 }
 
-int Board::CountLiberties(const int& n){
+int Board::CountLiberties(const int& n, std::vector<std::optional<Stone>>& local_board){
     std::unordered_set<int> liberties;
     std::unordered_set<int> visitedPositions;
-    std::optional<Stone> colour = board[n];
-    FindLiberties(n, colour, liberties, visitedPositions);
+    std::optional<Stone> colour = local_board[n];
+    FindLiberties(n, colour, liberties, visitedPositions, local_board);
     return liberties.size();
 }
 
-std::unordered_set<int> Board::GetGroup(const int& n){
+std::unordered_set<int> Board::GetGroup(const int& n, std::vector<std::optional<Stone>>& local_board){
     std::unordered_set<int> groupPositions;
     std::unordered_set<int> visitedPositions;
-    std::optional<Stone> colour = board[n];
-    FindGroup(n, colour, groupPositions, visitedPositions);
+    std::optional<Stone> colour = local_board[n];
+    FindGroup(n, colour, groupPositions, visitedPositions, local_board);
     return groupPositions;
 }
 
-bool Board::ValidMove(const int& n, std::optional<Stone> colour){ // check if valid move with a copy of the board
-    std::vector<std::optional<Stone>> temp_board = board;
-    if(n<0 && n > boardSize*boardSize-1) return false;
-    if(temp_board[n]!=std::nullopt) return false;
-    temp_board[n] = colour; // temporary placement
+bool Board::ValidMove(const int& n, std::optional<Stone> colour, std::vector<std::optional<Stone>>& local_board){ // check if valid move with a copy of the board
+    if(n<0 || n > boardSize*boardSize-1) return false;
+    if(board[n]!=std::nullopt) return false;
+    local_board[n] = colour; // temporary placement
     for(int neighbourPos:neighbours[n]){
         //check capture
-        if(temp_board[neighbourPos]!=std::nullopt && temp_board[neighbourPos]!=colour && CountLiberties(neighbourPos, temp_board)==0){
-            for(int opponentGroup:GetGroup(neighbourPos, temp_board)){
-                temp_board[opponentGroup]=std::nullopt;
+        if(local_board[neighbourPos]!=std::nullopt && local_board[neighbourPos]!=colour && CountLiberties(neighbourPos, local_board)==0){
+            for(int opponentGroup:GetGroup(neighbourPos, local_board)){
+                local_board[opponentGroup]=std::nullopt;
             }
         }
     }
-    if(CountLiberties(n)>0) return true;
+    if(CountLiberties(n, local_board)>0) return true;
     else return false;
 };
 
 void Board::PlayStone(const int& n, std::optional<Stone> colour){ // update the board with the valid move and captures
-    if(ValidMove(n, colour)) {
-        board[n] = colour;
+    std::vector<std::optional<Stone>> temp_board = board;
+    if(ValidMove(n, colour, temp_board)){
+        board = temp_board;
     }
 };
