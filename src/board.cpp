f7a562417ca0b532f15809283ea93ebfd49b742a
carys <caryshcleung@gmail.com>
2026-04-24 20:04:42 -0300

    improving efficiency

--- src/board.cpp
+++ src/board.cpp
@@ -27,61 +27,64 @@ int Board::CoordinateToIndex(const int& x, const int& y){
     return y * Board::boardSize + x;
 }
 
-void Board::ExploreBoard(const int&n, const std::optional<Stone>& colour, BoardInfo& boardInfo, std::unordered_set<int>& visitedPositions, std::vector<std::optional<Stone>>& local_board){
+void Board::ExploreBoard(const int&n, const std::optional<Stone>& colour, BoardInfo& boardInfo, std::unordered_set<int>& visitedPositions){
     if(colour==std::nullopt) return;
     if(!visitedPositions.insert(n).second) return; // check if node is visited
     boardInfo.groupPositions.insert(n);
     for (int neighbourPos:neighbours[n]){ // check neighbouring stones
-        if (local_board[neighbourPos]==std::nullopt){ // empty spot?
+        if (board[neighbourPos]==std::nullopt){ // empty spot?
             boardInfo.liberties.insert(neighbourPos);
         }
-        else if (local_board[neighbourPos]==colour){
-            ExploreBoard(neighbourPos, colour, boardInfo, visitedPositions, local_board);
+        else if (board[neighbourPos]==colour){
+            ExploreBoard(neighbourPos, colour, boardInfo, visitedPositions, board);
         }
     }
 }
 
-BoardInfo Board::GetInfo(const int& n, std::vector<std::optional<Stone>>& local_board){
+BoardInfo Board::GetInfo(const int& n){
     BoardInfo boardInfo;
     std::unordered_set<int> visitedPositions;
-    std::optional<Stone> colour = local_board[n];
-    ExploreBoard(n, colour, boardInfo, visitedPositions, local_board);
+    std::optional<Stone> colour = board[n];
+    ExploreBoard(n, colour, boardInfo, visitedPositions);
     return boardInfo;
 }
 
 // simulate move regardless of legality
-std::vector<std::optional<Stone>> Board::SimulateMove(const int& n, std::optional<Stone> colour, std::vector<std::optional<Stone>> local_board){
-    local_board[n]=colour;
-    std::unordered_set<int> visitedPos;
+std::vector<int> Board::SimulateMove(const int& n, std::optional<Stone> colour){
+    board[n]=colour;
+    std::vector<char> visitedPos(boardSize*boardSize, 0);
+    std::vector<int> changedPos; // finish changedPos
+    changedPos.push_back(n);
     for(int neighbourPos:neighbours[n]){
         // only check neighbouring stones with opposite colour for capture opportunity
-        if (local_board[neighbourPos]==std::nullopt || local_board[neighbourPos]==colour || visitedPos.find(neighbourPos)!=visitedPos.end()){
+        if (board[neighbourPos]==std::nullopt || board[neighbourPos]==colour || visitedPos[neighbourPos]==1){
             continue;
         }
-        BoardInfo neighbourInfo = GetInfo(neighbourPos, local_board);
+        BoardInfo neighbourInfo = GetInfo(neighbourPos);
         for (int pos:neighbourInfo.groupPositions){
-            visitedPos.insert(pos);
+            visitedPos[pos]=1;
         }
         if (neighbourInfo.liberties.empty()){
             for (int pos:neighbourInfo.groupPositions){
-                local_board[pos] = std::nullopt; // capture the whole group
+                board[pos] = std::nullopt; // capture the whole group
+                changedPos.push_back(pos);
             }
         }
     }
-    return local_board;
+    return changedPos;
 }
 
 // validate simulated move
-bool Board::ValidMove(const int& n, std::vector<std::optional<Stone>>& local_board){ // check if valid move with a copy of the board
-    if(GetInfo(n, local_board).liberties.size()>0) return true;
+bool Board::ValidMove(const int& n, std::vector<int>& changePos){ // check if valid move with a copy of the board
+    if(GetInfo(n).liberties.size()>0) return true;
     else return false;
 }
 
 void Board::PlayStone(const int& n, std::optional<Stone> colour){ // update the board with the valid move and captures
     if(n < 0 || n >= boardSize*boardSize) return;
     if(board[n]!=std::nullopt) return;
-    std::vector<std::optional<Stone>> temp_board = SimulateMove(n, colour, board);
-    if (ValidMove(n, temp_board)){
-        board = temp_board;
+    std::vector<int> changedPos = SimulateMove(n, colour);
+    if (!ValidMove(n, changePos)){
+        // TODO: undo changes logic here
     }
 };
